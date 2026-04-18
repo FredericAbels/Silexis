e8537044ae6f119bbfc99c2404b15041315d995b
Frederic <Abels>
2026-04-18 16:04:48 +0200

    did stuff

--- src/main.py
+++ src/main.py
@@ -8,10 +8,12 @@ def main():
 
     if len(digits) is 2:
         sildict["X" * 2][digits[0] + "X"][tier][perm] = "":
+
+    # check if word already exists
         
 
 def load_dict():
-    with open("../Silexian\ Dictionary.json", "r") as file:
+    with open("../silexicon/silexian\ dictionary.json", "r") as file:
         return json.load(file)        
 
 if __name__ == "__main__":
