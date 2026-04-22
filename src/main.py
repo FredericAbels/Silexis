675e31f8d5ace5752ecc4d12afdd002e7f8798ad
Frederic <Abels>
2026-04-22 08:52:01 +0200

    refactored

--- src/main.py
+++ src/main.py
@@ -1,71 +1,7 @@
 import json
 import random
-
-def main():
-    path_sildict = "../silexicon/dictionary.json"
-    sildict = load_dict(path_sildict)
-    path_phonemes = "../silexicon/phonemes.json"
-    phondict = load_dict(path_phonemes)
-
-    example1 = "example 60 4"
-    example2 = "example 8245 6"
-
-    print("Options:")
-    print("0 convert from add_to_dict.json")
-    print("1 [eng] [ds] [tier]")
-    print("2 " + example1)
-    print("3 " + example2)
-
-    cmd = input()
-    if cmd == "2":
-        cmd = "2 " + example1
-    if cmd == "3":
-        cmd = "3 " + example2
-
-    split_cmd = cmd.split()
-
-    if split_cmd[0] in ["1", "2", "3"]:
-        cmd, eng, ds, tier = split_cmd
-
-        if len(ds) == 2:
-            while True:
-                np1 = random.randint(1, int(tier) - 1)
-                np2 = int(tier) - np1
-                div = f"{np1}{np2}"
-                code = to_rand_sil_code(ds, tier, [np1, np2])
-                sil = code_to_sil_XX(ds, [np1, np2], code, phondict)
-
-                node = ensure_path(sildict["XX"], ds[0]+"X", ds, "T"+tier, "NP"+div)
-                if "P"+code not in node:
-                    ans = input(f"How about {sil} for {eng}? yes, continue or quit? ")
-                    if ans in ("y", "yes"):
-                        node["P"+code] = {"sil": sil, "eng": eng.replace("_", " ")}
-                        break
-                    elif ans in ("quit", "q"):
-                        break
-
-        elif len(ds) == 4:
-            while True:
-                np1 = random.randint(1, int(tier) - 3)
-                np2 = random.randint(1, int(tier) - np1 - 2)
-                np3 = random.randint(1, int(tier) - (np1 + np2) - 1)
-                np4 = int(tier) - (np1 + np2 + np3)
-                div = f"{np1}{np2}{np3}{np4}"
-                code = to_rand_sil_code(ds, tier, [np1, np2, np3, np4])
-                sil = code_to_sil_XXXX(ds, [np1, np2, np3, np4], code, phondict)
-
-                node = ensure_path(sildict["XXXX"], ds[0]+"XXX", ds[:2]+"XX", ds[:3]+"X", ds, "T"+tier, "NP"+tier)
-                if "P"+code not in node:
-                    print(f"ds: {ds}, div: {div}, code: {code}")
-                    ans = input(f"How about {sil} for {eng}? yes, continue or quit? ")
-                    if ans in ("y", "yes"):
-                        node["P"+code] = {"sil": sil, "eng": eng.replace("_", " ")}
-                        break
-                    elif ans in ("quit", "q"):
-                        break
-
-    with open(path_sildict, "w") as f:
-        json.dump(sildict, f, indent=4)
+path_sildict = "../silexicon/dictionary.json"
+path_phonemes = "../silexicon/phonemes.json"
 
 def ensure_path(d, *keys):
     for key in keys:
@@ -74,7 +10,93 @@ def ensure_path(d, *keys):
 
 def load_dict(file_path):
     with open(file_path, "r") as file:
-        return json.load(file)        
+        return json.load(file)   
+
+
+def main():
+    sildict = load_dict(path_sildict)
+    phondict = load_dict(path_phonemes)
+
+    example1 = "example 60 4"
+    example2 = "example 8245 6"
+
+    print("Options:")
+    print("convert0 (from add_to_dict.json)")
+    print("add0 [eng] [ds] [tier]")
+    print("add1 " + example1)
+    print("add2 " + example2)
+    print("lookup0 [eng]")
+
+    cmd = input()
+    if cmd == "add1":
+        cmd = "add1 " + example1
+    if cmd == "add2":
+        cmd = "add2 " + example2
+
+    scmd = cmd.split()
+
+    if scmd[0][:-1] == "lookup":
+        print()
+
+    if scmd[0][:-1] == "add":
+        add_term_to_dict(scmd, sildict, phondict)
+
+
+def add_term_to_dict(scmd, sildict, phondict):
+    cmd, eng, ds, tier = scmd
+
+    if len(ds) == 2:
+        sildict = make_XX_sil_and_add(cmd, eng, ds, tier, sildict, phondict)
+            
+    elif len(ds) == 4:
+        sildict = make_XX_sil_and_add(cmd, eng, ds, tier, sildict, phondict)
+
+    with open(path_sildict, "w") as f:
+        json.dump(sildict, f, indent=4)
+
+def make_XX_sil_and_add(cmd, eng, ds, tier, sildict, phondict):
+
+    while True:
+        np1 = random.randint(1, int(tier) - 1)
+        np2 = int(tier) - np1
+        div = f"{np1}{np2}"
+        code = to_rand_sil_code(ds, tier, [np1, np2])
+        sil = p_code_to_sil_XX(ds, [np1, np2], code, phondict)
+
+        node = ensure_path(sildict["XX"], ds[0]+"X", ds, "T"+tier, "NP"+div)
+        if "P"+code not in node:
+            print(f"ds: {ds}, div: {div}, code: {code}")
+            ans = input(f"How about {sil} for {eng}? yes, continue or quit? ")
+            if ans in ("y", "yes"):
+                node["P"+code] = {"sil": sil, "eng": eng.replace("_", " ")}
+                break
+            elif ans in ("quit", "q"):
+                break
+    
+    return sildict
+
+def make_XXXX_sil_and_add(cmd, eng, ds, tier, sildict, phondict):
+
+    while True:
+        np1 = random.randint(1, int(tier) - 3)
+        np2 = random.randint(1, int(tier) - np1 - 2)
+        np3 = random.randint(1, int(tier) - (np1 + np2) - 1)
+        np4 = int(tier) - (np1 + np2 + np3)
+        div = f"{np1}{np2}{np3}{np4}"
+        code = to_rand_sil_code(ds, tier, [np1, np2, np3, np4])
+        sil = p_code_to_sil_XXXX(ds, [np1, np2, np3, np4], code, phondict)
+
+        node = ensure_path(sildict["XXXX"], ds[0]+"XXX", ds[:2]+"XX", ds[:3]+"X", ds, "T"+tier, "NP"+tier)
+        if "P"+code not in node:
+            print(f"ds: {ds}, div: {div}, code: {code}")
+            ans = input(f"How about {sil} for {eng}? yes, continue or quit? ")
+            if ans in ("y", "yes"):
+                node["P"+code] = {"sil": sil, "eng": eng.replace("_", " ")}
+                break
+            elif ans in ("quit", "q"):
+                break
+
+    return sildict
 
 def to_rand_sil_code(ds, tier, np_list):
     silcode = ""
@@ -87,7 +109,7 @@ def to_rand_sil_code(ds, tier, np_list):
             j = j - 1
     return silcode
 
-def code_to_sil_XX(ds, np_list, code, phondict):
+def p_code_to_sil_XX(ds, np_list, code, phondict):
     sil = ""
     for i in range(0, sum(np_list)):
 
@@ -106,7 +128,7 @@ def code_to_sil_XX(ds, np_list, code, phondict):
 
     return sil
 
-def code_to_sil_XXXX(ds, np_list, code, phondict):
+def p_code_to_sil_XXXX(ds, np_list, code, phondict):
     sil = ""
     for i in range(0, sum(np_list)):
 
@@ -134,7 +156,6 @@ def code_to_sil_XXXX(ds, np_list, code, phondict):
         sil = sil + phon
 
     return sil
-
 
 if __name__ == "__main__":
     main()
